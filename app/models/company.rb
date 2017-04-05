8f386ed581d7b8c1185ef989fdd0c69908ca027b
Ryan Odening <ryanodening@Ryans-MacBook-Pro.local>
2017-04-04 19:01:53 -0700

    update user cash in holding model

--- app/models/company.rb
+++ app/models/company.rb
@@ -10,10 +10,23 @@
 #  updated_at  :datetime         not null
 #
 
+require 'open-uri'
+require 'nokogiri'
+
 class Company < ApplicationRecord
   validates :name, :symbol, :share_price, presence: true
   validates :name, :symbol, uniqueness: true
 
   has_many :holdings
   has_many :shareholders, through: :holdings, source: :user
+
+  def self.find_value(symbol)
+   url = "https://www.google.com/finance/getprices?i=60&p=1d&f=c&q=#{symbol}"
+   doc = Nokogiri::HTML(open(url))
+   stock_full_info = doc.css('pre')
+   debugger
+   stock_prices = stock_full_info[0].outerText.split(/\n/)
+   current_price = stock_prices[stock_prices.length - 2]
+   current_price
+ end
 end
--- app/models/holding.rb
+++ app/models/holding.rb
@@ -19,14 +19,31 @@ class Holding < ApplicationRecord
 
   def buy(amount)
     self.update_attribute(:amount, self.amount+amount)
+    user = User.find(self.user_id)
+    company = Company.find(self.company_id)
+    #update company share price here (live http request)
+    user.update_attribute(:cash_on_hand, user.cash_on_hand - amount * company.share_price)
+    #here could be some return value
   end
+
+  #could be renamed to owner; in that case other instances of user...
+  #... should prob be changed to owner as well
+  # def user
+  #   User.find(self.user_id)
+  # end
 
   def sell(amount)
     if amount > self.amount
       throw "Can't sell more than you own!"
     else
       self.update_attribute(:amount, self.amount - amount)
+      user = User.find(self.user_id)
+      company = Company.find(self.company_id)
+      #update company share price here (live http request)
+      user.update_attribute(:cash_on_hand, user.cash_on_hand + amount * company.share_price)
     end
+    #code for updating user can be cleaned up and/or abstracted
+    #some return value
   end
 
 end

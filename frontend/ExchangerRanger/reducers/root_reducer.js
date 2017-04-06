b9c3f513f9ad166efabed88230554087b9b0e71e
Ryan Odening <ryanodening@Ryans-MacBook-Pro.local>
2017-04-06 11:41:36 -0700

    finish (draft) redux cycle sin user

--- frontend/ExchangerRanger/reducers/root_reducer.js
+++ frontend/ExchangerRanger/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import SessionReducer from './session_reducer';
+import StocksReducer from './stocks_reducer';
 
 const RootReducer = combineReducers({
-  session: SessionReducer
+  session: SessionReducer,
+  stocks: StocksReducer
 });
 
 export default RootReducer;
--- frontend/ExchangerRanger/reducers/stocks_reducer.js
+++ frontend/ExchangerRanger/reducers/stocks_reducer.js
@@ -1,32 +1,26 @@
-// import { CREATE_REVIEW,
-//   RECEIVE_REVIEW,
-//   RECEIVE_BOOKING,
-//   RECEIVE_SPOTS,
-//   RECEIVE_SPOT } from '../actions/spot_actions';
-//
-// import merge from 'lodash/merge';
-//
-// const SpotsReducer = (state = {}, action) => {
-//   Object.freeze(state)
-//   let newState = merge({}, state);
-//
-//   switch(action.type) {
-//     case RECEIVE_SPOTS:
-//       return action.spots;
-//     case RECEIVE_SPOT:
-//       const newSpot = {[action.spot.id]: action.spot};
-//       return merge({}, state, newSpot);
-//     case RECEIVE_REVIEW:
-//       const review = action.review;
-//       newState[review.spot_id].reviews.push(review)
-//       return newState;
-//     case RECEIVE_BOOKING:
-//       const booking = action.booking;
-//       newState[booking.spot_id].bookings.push(booking)
-//       return newState;
-//     default:
-//       return state;
-//   }
-// };
-//
-// export default SpotsReducer;
+import { RECEIVE_COMPANIES,
+RECEIVE_COMPANY,
+RECEIVE_HOLDING } from '../actions/stock_actions';
+
+import merge from 'lodash/merge';
+
+const CompaniesReducer = (state = {}, action) => {
+  Object.freeze(state)
+  let newState = merge({}, state);
+
+  switch(action.type) {
+    case RECEIVE_COMPANIES:
+      return action.companies;
+    case RECEIVE_COMPANY:
+      const newCompany = {[action.company.id]: action.company};
+      return merge({}, state, newCompany);
+    case RECEIVE_HOLDING:
+      const holding = action.holding;
+      newState[holding.company_id].holdings[holding.id]=holding;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default CompaniesReducer;

601f98f3b8e68d80ec14ec89c3948dc4c3504ed4
Andrea del Rio <adelrio@gmail.com>
2017-04-06 12:04:04 -0700

    reset db

--- db/schema.rb
+++ db/schema.rb
@@ -47,12 +47,12 @@ ActiveRecord::Schema.define(version: 20170404160813) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "password_digest", null: false
-    t.string   "session_token",   null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["password_digest"], name: "index_users_on_password_digest"
+    t.string   "username",                         null: false
+    t.string   "password_digest",                  null: false
+    t.string   "session_token",                    null: false
+    t.integer  "cash_on_hand",    default: 100000, null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.index ["session_token"], name: "index_users_on_session_token"
     t.index ["username"], name: "index_users_on_username"
   end

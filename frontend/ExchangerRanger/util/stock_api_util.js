5f7e3d3fa4d5da291e289672d014e98f6ada7663
Andrea del Rio <adelrio@gmail.com>
2017-04-08 14:58:27 -0700

    recommiting fetch actions for stock

--- frontend/ExchangerRanger/util/stock_api_util.js
+++ frontend/ExchangerRanger/util/stock_api_util.js
@@ -1,47 +1,91 @@
 
 export const fetchCompanies = (data) => {
-  return $.ajax({
+  return fetch('http://localhost:3000/api/companies', {
     method: 'GET',
-    url: 'api/companies',
-    data
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: 'data.name',
+      symbol: 'data.symbol',
+      sharePrice: 'data.share_price'
+    })
   });
 };
 
 export const fetchCompany = (id) => {
-  return $.ajax({
+  return fetch(`http://localhost:3000/api/companies/${id}`, {
     method: 'GET',
-    url: `api/companies/${id}`
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: 'data.name',
+      symbol: 'data.symbol',
+      sharePrice: 'data.share_price'
+    })
   });
+
 };
 //make new holding
 export const createHolding = (data) => {
-  return $.ajax({
+  return fetch(`http://localhost:3000/api/holdings`, {
     method: 'POST',
-    url: 'api/holdings',
-    data
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      userId: 'data.user_id',
+      companyId: 'data.company_id',
+      amount: 'data.amount'
+    })
   });
 };
 
 export const receiveHolding = (data) => {
-  return $.ajax({
+  return fetch(`http://localhost:3000/api/holdings/${data.id}`, {
     method: 'GET',
-    url: `api/holdings/${data.id}`,
-    data
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      userId: 'data.user_id',
+      companyId: 'data.company_id',
+      amount: 'data.amount'
+    })
   });
 };
 //change holding amount
 export const updateHolding = (data) => {
-  return $.ajax({
+  return fetch(`http://localhost:3000/api/holdings/${data.id}`, {
     method: 'PATCH',
-    url: `api/holdings/${data.id}`,
-    data
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      userId: 'data.user_id',
+      companyId: 'data.company_id',
+      amount: 'data.amount'
+    })
   });
 };
 //update company price
-export const updatePrice = (data) => {
-  return $.ajax({
+export const updatePrice = (company) => {
+  return fetch(`http://localhost:3000/api/companies/${company.id}`, {
     method: 'PATCH',
-    url: `api/companies/${data.id}`,
-    data
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: 'data.name',
+      symbol: 'data.symbol',
+      sharePrice: 'data.share_price'
+    })
   });
 };

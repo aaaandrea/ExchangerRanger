e6b3b21e3f953e2c140eb9290389b931c1148b78
Andrea del Rio <adelrio@gmail.com>
2017-04-09 16:13:40 -0700

    add users.rb file to add new users

---
+++ db/users.rb
@@ -0,0 +1,17 @@
+$users = "
+Andrea
+Ryan
+Aaron
+TopDawg
+NextTimeWillBeBetter
+$$$$
+HiMom
+Josh
+BenFrank
+TwoSense
+IAmTheWalrus
+IAmTheEggMan
+TheDoughBoy
+WageRider
+RevenueAvenue
+".split("\n")

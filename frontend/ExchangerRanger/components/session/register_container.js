2ed37b58d06d7c985e44e56720c2a9a017b74533
Andrea del Rio <adelrio@gmail.com>
2017-04-09 23:36:27 -0700

    update register container

--- frontend/ExchangerRanger/components/session/register_container.js
+++ frontend/ExchangerRanger/components/session/register_container.js
@@ -5,27 +5,16 @@ import Register from './register';
 import React, {Component} from 'react-native';
 
 const mapStateToProps = (state) =>   {
-
-  return ({loggedIn: Boolean(state.session.currentUser),
-  errors: state.session.errors});
-  // stocks: Object.keys(state.stocks).map(id => state.stocks[id])});
+  return ({
+    loggedIn: Boolean(state.session.currentUser),
+    errors: state.session.errors
+  });
 };
 
 const mapDispatchToProps = (dispatch, {location}) => ({
-
-  //ask for explanation about this one...
-  // let formType = location.pathname.slice(1);
-  // const processForm = (formType === 'login') ? login : signup;
-  //
-  // return {
-  //   processForm: user => dispatch(processForm(user)),
-  //   formType
-  // };
   login: (user) => dispatch(login(user)),
   signup: (user) => dispatch(signup(user)),
   logout: () => dispatch(logout())
-  // updatePrice: company => dispatch(updatePrice(company))
-
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Register);

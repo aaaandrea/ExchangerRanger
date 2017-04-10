7455449aaba4e84eccd17af762202fbf1a3c6262
Andrea del Rio <adelrio@gmail.com>
2017-04-10 01:28:31 -0700

    remove unnecessary buttons form splash page

--- frontend/ExchangerRanger/splash.js
+++ frontend/ExchangerRanger/splash.js
@@ -70,28 +70,6 @@ export default class Splash extends Component {
             </Text>
           </View>
         </TouchableHighlight>
-
-        <TouchableHighlight
-          onPress={() => this.props.navigator.push({id: 'StockIndex'})}
-          underlayColor='#FFFFFE'
-          activeOpacity={0.7}>
-          <View style={styles.buttonContainer}>
-            <Text style={styles.button}>
-              StockIndex
-            </Text>
-          </View>
-        </TouchableHighlight>
-
-        <TouchableHighlight
-          onPress={() => this.props.navigator.push({id: 'Leaderboard'})}
-          underlayColor='#FFFFFE'
-          activeOpacity={0.7}>
-          <View style={styles.buttonContainer}>
-            <Text style={styles.button}>
-              Leaderboard
-            </Text>
-          </View>
-        </TouchableHighlight>
       </View>
     );
   }
